99b674c9154735f54315b9085b2e79ff39a93688
Rafadluz <rafaela.l.santos8@aluno.senai.br>
2026-04-06 16:45:52 -0300

    revisão js

---
+++ revisao/revisao.js
@@ -0,0 +1,18 @@
+function revisao(){
+    let nome = "Aleatorio";
+    console.log(nome);
+    if (true){
+        let sobrenome = "ata";
+        console.log(sobrenome);
+    }
+    const idade = 20;
+    console.log(idade);
+
+    const precoOriginal = 100;
+    const desconto = 20;
+    let precoFinal = precoOriginal - desconto;
+
+    console.log("Preço Original: " + precoOriginal)
+    console.log
+}
+revisao()
---
+++ revisao/strings.js
@@ -0,0 +1,3 @@
+let texto = "Alô, mundo!"
+console.log(texto.length)
+console.log(texto[0]);
